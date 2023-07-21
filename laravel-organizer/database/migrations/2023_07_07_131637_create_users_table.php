456b95c3ff40490633829b2441baac0542e45a57
ThisSurya <suryamichael09@gmail.com>
2023-07-21 21:08:07 +0700

    check if admin or not

--- laravel-organizer/database/migrations/2023_07_07_131637_create_users_table.php
+++ laravel-organizer/database/migrations/2023_07_07_131637_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->foreignId('role_id')->default('2')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('password');
+            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
